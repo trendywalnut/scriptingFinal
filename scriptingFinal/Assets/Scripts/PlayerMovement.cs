72baf301951aac2a0c9b7daeb687eb942525459b
jojohorak <jehorak@utexas.edu>
2020-11-29 13:26:31 -0600

    simple click to move

---
+++ scriptingFinal/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public LayerMask whatCanBeClickedOn;
+
+    private NavMeshAgent myAgent;
+
+    private void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(myRay, out hitInfo, 100, whatCanBeClickedOn))
+            {
+                myAgent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+
+}
