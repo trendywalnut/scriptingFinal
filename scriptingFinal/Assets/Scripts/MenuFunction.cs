437e55186390f8ae296eebbaf5dbe5698e814834
trendywalnut <connorb23@utexas.edu>
2020-12-02 10:43:23 -0600

    menu scene

---
+++ scriptingFinal/Assets/Scripts/MenuFunction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuFunction : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit test");
+    }
+}
