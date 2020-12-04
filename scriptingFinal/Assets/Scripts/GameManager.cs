76d0f8f5332944abcccb18baccc9c6cfd35f5987
trendywalnut <connorb23@utexas.edu>
2020-12-03 21:57:10 -0600

    my scene and then gamemanager

---
+++ scriptingFinal/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private int health = 69;
+    private bool slickwitit = false;
+    public int integer;
+
+    void Awake()
+    {
+        if (FindObjectsOfType<GameManager>().Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+
+    void Update()
+    {
+
+    }
+}
