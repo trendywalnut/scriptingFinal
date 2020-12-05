ce502e91f448edfbacdeeb4e42a74b116a1ea266
Assrael <71477547+Assrael@users.noreply.github.com>
2020-12-05 16:18:42 -0600

    dialouge improvements

---
+++ Assets/Scripts/DialougeSaver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialougeSaver : MonoBehaviour
+{
+    void Awake()
+    {
+        if (FindObjectsOfType<DialougeSaver>().Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
