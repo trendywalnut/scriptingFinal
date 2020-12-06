22bac99a17f85f505af9787002eee6d0ca835a69
Assrael <71477547+Assrael@users.noreply.github.com>
2020-12-06 17:19:46 -0600

    minigame something

---
+++ Assets/GoToGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class GoToGame : MonoBehaviour
+{
+    [YarnCommand("")]
+    public void GoToMiniGame()
+    {
+
+    }
+}
