10bbdfea81c9933e854445ec91b8e91c134f6f18
trendywalnut <connorb23@utexas.edu>
2020-12-05 20:20:55 -0600

    Revert "idk bruh"
    This reverts commit 77b46b0ef319e88ce8ed2336a0af84fb23805d27.

---
+++ Assets/Scripts/CustomVariableStorage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn;
+
+public class CustomVariableStorage : Yarn.Unity.VariableStorageBehaviour
+{
+    // Store a value into a variable
+    public override void SetValue(string variableName, Value value)
+    {
+        // 'variableName' is the name of the variable that 'value' 
+        // should be stored in.
+        throw new System.NotImplementedException();
+    }
+
+    // Return a value, given a variable name
+    public override Value GetValue(string variableName)
+    {
+        // 'variableName' is the name of the variable to return a value for
+        throw new System.NotImplementedException();
+    }
+ 
+    // Return to the original state
+    public override void ResetToDefaults()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("player detect");
+            if (this.gameObject.tag == "ToPath")
+            {
+                SceneManager.LoadScene(2);
+                Debug.Log("it is soemthing");
+            }
+            else if (this.gameObject.tag == "ToWitch")
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+}
