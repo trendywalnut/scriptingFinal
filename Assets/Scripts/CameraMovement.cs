028c289c0aa18f41b3f5e40ec3cf08f93270f29c
trendywalnut <connorb23@utexas.edu>
2020-12-05 19:35:58 -0600

    lake of fire

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,42 +4,15 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Transform player;
-
-    public float playerX;
-    public float startingY;
-    public float startingZ;
-
-    private Vector3 goalPos;
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        calcPos();
-        transform.position = goalPos;
-
+        
     }
 
-    void LateUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        calcPos();
-        transform.position = Vector3.Lerp(transform.position, goalPos, 0.1f);
-    }
-
-    void calcPos()
-    {
-        playerX = player.transform.position.x;
-
-        if (playerX >= -44)
-        {
-            goalPos = new Vector3(-44, startingY, startingZ);
-        }
-        else if (playerX <= -57)
-        {
-            goalPos = new Vector3(-57, startingY, startingZ);
-        }
-        else
-        {
-            goalPos = new Vector3(playerX, startingY, startingZ);
-        }
+        
     }
 }
